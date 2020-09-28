863b8b68032d36120cc32c16eaab99540d23588f
gabriele <gc787618@ohio.edu>
2020-09-28 15:33:28 -0400

    finished

--- avl_tree.cpp
+++ avl_tree.cpp
@@ -2,15 +2,19 @@
 #include "avl_tree.h"
 using namespace std;
 
+
 void AVLTree::insert(const int key){
+	//call insert funtion passing the tree and key
 	this->root = insert(this->root, key);
 }
 
 void AVLTree::deleteNode(const int key){
+	//call delete node funtion passing the tree and key
 	this->root = deleteNode(this->root, key);
 }
 
 void AVLTree::print(const char c){
+	//call print function for height or key base on c
 	if(c == 'h'){
 		inOrderHeight(this->root);
 	}else if(c == 'p'){
@@ -19,279 +23,178 @@ void AVLTree::print(const char c){
 	cout << endl;
 }
 
-int AVLTree::height(Node *N) 
-{ 
+int AVLTree::height(Node *N){ 
+	//return height of the tree
 	if (N == NULL) 
 		return 0; 
 	return N->height; 
 } 
 
-// A utility function to get maximum 
-// of two integers 
-int AVLTree::max(int a, int b) 
-{ 
+int AVLTree::max(int a, int b){
+	// A function to get maximum  of two integer
 	return (a > b)? a : b; 
 } 
 
-/* Helper function that allocates a 
-new node with the given key and 
-NULL left and right pointers. */
-/*Node* AVLTree::newNode(int key) 
-{ 
-	Node* node = new Node(); 
-	node->key = key; 
-	node->left = NULL; 
-	node->right = NULL; 
-	node->height = 1; // new node is initially 
-					// added at leaf 
-	return(node); 
-} */
-
-// A utility function to right 
-// rotate subtree rooted with y 
-// See the diagram given above. 
-Node *AVLTree::rightRotate(Node *y) 
-{ 
+Node *AVLTree::rightRotate(Node *y){
+	//function to right rotate subtree with y 
 	Node *x = y->left; 
 	Node *T2 = x->right; 
 
-	// Perform rotation 
+	//rotation 
 	x->right = y; 
 	y->left = T2; 
 
-	// Update heights 
-	y->height = max(height(y->left), 
-					height(y->right)) + 1; 
-	x->height = max(height(x->left), 
-					height(x->right)) + 1; 
-
-	// Return new root 
+	//heights 
+	y->height = max(height(y->left),height(y->right)) + 1; 
+	x->height = max(height(x->left), height(x->right)) + 1; 
 	return x; 
 } 
 
-// A utility function to left 
-// rotate subtree rooted with x 
-// See the diagram given above. 
-Node *AVLTree::leftRotate(Node *x) 
-{ 
+Node *AVLTree::leftRotate(Node *x){
+	//function to left rotate subtree with x 
 	Node *y = x->right; 
 	Node *T2 = y->left; 
 
-	// Perform rotation 
+	//rotation 
 	y->left = x; 
 	x->right = T2; 
 
-	// Update heights 
-	x->height = max(height(x->left), 
-					height(x->right)) + 1; 
-	y->height = max(height(y->left), 
-					height(y->right)) + 1; 
-
-	// Return new root 
+	//heights 
+	x->height = max(height(x->left),height(x->right)) + 1; 
+	y->height = max(height(y->left),height(y->right)) + 1; 
 	return y; 
 } 
 
-// Get Balance factor of node N 
-int AVLTree::getBalance(Node *N) 
-{ 
+ 
+int AVLTree::getBalance(Node *N) { 
+	//balance factor of node N
 	if (N == NULL) 
 		return 0; 
 	return height(N->left) - 
 		height(N->right); 
 } 
 
-Node* AVLTree::insert(Node* node, int key) 
-{ 
-	/* 1. Perform the normal BST rotation */
+Node* AVLTree::insert(Node* node, int key){ 
+	//performe the normal BST rotation
 	if (node == NULL) 
 		return new Node(key); 
-
 	if (key < node->key) 
 		node->left = insert(node->left, key); 
 	else if (key > node->key) 
 		node->right = insert(node->right, key); 
-	else // Equal keys not allowed 
+	else 
 		return node; 
 
-	/* 2. Update height of this ancestor node */
-	node->height = 1 + max(height(node->left), 
-						height(node->right)); 
+	//update height of this ancestor node
+	node->height = 1 + max(height(node->left),height(node->right)); 
 
-	/* 3. Get the balance factor of this 
-		ancestor node to check whether 
-		this node became unbalanced */
+	//get the balance factor of this ancestor node to check whether this node became unbalanced
 	int balance = getBalance(node); 
 
-	// If this node becomes unbalanced, 
-	// then there are 4 cases 
-
-	// Left Left Case 
+	//left left case 
 	if (balance > 1 && key < node->left->key) 
 		return rightRotate(node); 
 
-	// Right Right Case 
+	//right right case 
 	if (balance < -1 && key > node->right->key) 
 		return leftRotate(node); 
 
-	// Left Right Case 
+	//left right case 
 	if (balance > 1 && key > node->left->key) 
 	{ 
 		node->left = leftRotate(node->left); 
 		return rightRotate(node); 
 	} 
 
-	// Right Left Case 
+	//right left case 
 	if (balance < -1 && key < node->right->key) 
 	{ 
 		node->right = rightRotate(node->right); 
 		return leftRotate(node); 
 	} 
-
-	/* return the (unchanged) node pointer */
 	return node; 
 } 
 
-/* Given a non-empty binary search tree, 
-return the node with minimum key value 
-found in that tree. Note that the entire 
-tree does not need to be searched. */
-Node * AVLTree::minValueNode(Node* node) 
-{ 
+Node * AVLTree::minValueNode(Node* node){
+	//return the node with minimum key value found in that tree 
 	Node* current = node; 
-
-	/* loop down to find the leftmost leaf */
 	while (current->left != NULL) 
 		current = current->left; 
 
 	return current; 
 } 
-
-// Recursive function to delete a node 
-// with given key from subtree with 
-// given root. It returns root of the 
-// modified subtree. 
+ 
 Node* AVLTree::deleteNode(Node* root, int key) 
 { 
 	
-	// STEP 1: PERFORM STANDARD BST DELETE 
+	//perform standard BST delete 
 	if (root == NULL) 
 		return root; 
-
-	// If the key to be deleted is smaller 
-	// than the root's key, then it lies 
-	// in left subtree 
 	if ( key < root->key ) 
 		root->left = deleteNode(root->left, key); 
-
-	// If the key to be deleted is greater 
-	// than the root's key, then it lies 
-	// in right subtree 
 	else if( key > root->key ) 
 		root->right = deleteNode(root->right, key); 
-
-	// if key is same as root's key, then 
-	// This is the node to be deleted 
-	else
-	{ 
-		// node with only one child or no child 
-		if( (root->left == NULL) || 
-			(root->right == NULL) ) 
-		{ 
+	else{ 
+		//node with only one child or no child 
+		if( (root->left == NULL) || (root->right == NULL) ){ 
 			Node *temp = root->left ? 
 						root->left : 
 						root->right; 
 
 			// No child case 
-			if (temp == NULL) 
-			{ 
+			if (temp == NULL){ 
 				temp = root; 
 				root = NULL; 
 			} 
 			else // One child case 
-			*root = *temp; // Copy the contents of 
-						// the non-empty child 
-			free(temp); 
-		} 
-		else
-		{ 
-			// node with two children: Get the inorder 
-			// successor (smallest in the right subtree) 
+				*root = *temp; 
+				free(temp); 
+		}else{ 
 			Node* temp = minValueNode(root->right); 
-
-			// Copy the inorder successor's 
-			// data to this node 
 			root->key = temp->key; 
-
-			// Delete the inorder successor 
-			root->right = deleteNode(root->right, 
-									temp->key); 
+			root->right = deleteNode(root->right, temp->key); 
 		} 
 	} 
-
-	// If the tree had only one node 
-	// then return 
 	if (root == NULL) 
-	return root; 
+		return root; 
 
-	// STEP 2: UPDATE HEIGHT OF THE CURRENT NODE 
-	root->height = 1 + max(height(root->left), 
-						height(root->right)); 
+	//update height of the current node 
+	root->height = 1 + max(height(root->left),height(root->right)); 
 
-	// STEP 3: GET THE BALANCE FACTOR OF 
-	// THIS NODE (to check whether this 
-	// node became unbalanced) 
-	int balance = getBalance(root); 
+	//get the balance factor  
+	int balance = getBalance(root);  
 
-	// If this node becomes unbalanced, 
-	// then there are 4 cases 
-
-	// Left Left Case 
-	if (balance > 1 && 
-		getBalance(root->left) >= 0) 
+	//left left case 
+	if (balance > 1 && getBalance(root->left) >= 0) 
 		return rightRotate(root); 
-
-	// Left Right Case 
-	if (balance > 1 && 
-		getBalance(root->left) < 0) 
-	{ 
+	//left right case 
+	if (balance > 1 && getBalance(root->left) < 0){ 
 		root->left = leftRotate(root->left); 
 		return rightRotate(root); 
 	} 
-
-	// Right Right Case 
-	if (balance < -1 && 
-		getBalance(root->right) <= 0) 
+	//right right case 
+	if (balance < -1 && getBalance(root->right) <= 0) 
 		return leftRotate(root); 
-
 	// Right Left Case 
-	if (balance < -1 && 
-		getBalance(root->right) > 0) 
-	{ 
+	if (balance < -1 && getBalance(root->right) > 0){ 
 		root->right = rightRotate(root->right); 
 		return leftRotate(root); 
 	} 
-
 	return root; 
 } 
-
-// A utility function to print preorder 
-// traversal of the tree. 
-// The function also prints height 
-// of every node 
-void AVLTree::inOrderHeight(Node *root) 
-{ 
-	if(root != NULL) 
-	{ 
+ 
+void AVLTree::inOrderHeight(Node *root){ 
+	//print in order height of each node
+	if(root != NULL) { 
 		inOrderHeight(root->left);
         cout << root->height << " "; 
 		inOrderHeight(root->right); 
 	} 
 } 
 
-void AVLTree::inOrderKey(Node *root) 
-{ 
-	if(root != NULL) 
-	{ 
+void AVLTree::inOrderKey(Node *root){ 
+	//print in order key of each node
+	if(root != NULL) { 
 		inOrderKey(root->left);
         cout << root->key << " "; 
 		inOrderKey(root->right); 
--- avl_tree.h
+++ avl_tree.h
@@ -1,42 +1,62 @@
 #ifndef AVL_TREE_H
 #define AVL_TREE_H
 
-struct Node 
-{ 
+//An Avl Tree Node
+struct Node{ 
 	public: 
+  //Constructor
   Node(const int key) : key(key), height(1), left(NULL), right(NULL){}
 
+  //Destructor
   ~Node(){
     delete left;
     delete right;
     left = right = NULL;
   }
+
 	int key; 
 	Node *left; 
 	Node *right; 
 	int height; 
 };
 
+
 class AVLTree{
   public:
+    //Constructor
     AVLTree() : root(NULL) {}
+    //Desctructor 
     ~AVLTree(){
       delete root;
     }
+    //max between two variables
     int max(int a, int b);
+    //return height of the node
     int height(Node *N);
+    //get balnce factor of the node
     int getBalance(Node *N);
+    //print function
     void print(const char c);
+    //allocate a new node with key passed
     void insert(const int key);
+    //delete node key
     void deleteNode(const int key);
   private:
+    //allocate a new node with key passed
     Node* newNode(int key) ;
+    //right rotate the sub tree rooted with y
     Node *rightRotate(Node *y);
+    // left rotate the sub tree rooted with x
     Node *leftRotate(Node *x);
+    //insert new node in tree
     Node* insert(Node* node, int key);
+    //return the node with minimum key value found in that tree
     Node * minValueNode(Node* node);
+    //delete node with key passed
     Node* deleteNode(Node* root, int key);
+    //print in order the height of each node
     void inOrderHeight(Node *root);
+    //print in order the key of each node
     void inOrderKey(Node *root);
     Node* root;
 };
