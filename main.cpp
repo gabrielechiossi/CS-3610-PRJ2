30c32e2b07a2a7c96c52b537914be24731ec4147
gabriele <gc787618@ohio.edu>
2020-09-28 15:34:22 -0400

    finished

--- main.cpp
+++ main.cpp
@@ -2,28 +2,27 @@
 #include "avl_tree.h"
 using namespace std;
 
-int main() 
-{ 
-AVLTree root; 
-    int rm, value; 
-char option;
-  do{
-      cin >> option;
-      switch(option){
-          case 'i':
-            cin >> value;
-            root.insert(value);
-            break;
-          case 'r':
-            cin >> rm;
-            root.deleteNode(rm);
-            break;
-          case 'h':
-            root.print('h');
-            break;
-          case 'p':
-            root.print('p');
-      }
-  }while(option != 'q');
+int main(){ 
+  AVLTree root; 
+  int rm, value; 
+  char option;
+    do{
+        cin >> option;
+        switch(option){
+            case 'i':
+              cin >> value;
+              root.insert(value);
+              break;
+            case 'r':
+              cin >> rm;
+              root.deleteNode(rm);
+              break;
+            case 'h':
+              root.print('h');
+              break;
+            case 'p':
+              root.print('p');
+        }
+    }while(option != 'q');
 	return 0; 
 } 
